d11843a85b6a54f077e20f2cf8ffb883a74bfd81
Elvi3n <154032045+Elvi3n@users.noreply.github.com>
2025-12-13 15:14:05 -0800

    Delete scripts/08_ViewQueriesExamples.sql

--- scripts/08_ViewQueriesExamples.sql
+++
@@ -1,9 +0,0 @@
-USE DataWarehouseAnalytics;
-GO
-
-SELECT 
-	age_group,
-	COUNT(customer_number) AS total_customers,
-	SUM(total_sales) AS total_sales
-FROM gold.report_customers
-GROUP BY age_group
